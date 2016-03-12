e2a3cc616af157770b832847b3d0da6cf5e446c0
Fangfang Fu <fangfang@fufangfang.com>
2016-03-11 20:35:31 -0600

    Add room name,  make it deal with multiple rooms and send chat room JSON

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var app = express();
-var messages = [];
-app.post('/chatroom/:username', function(request, response){
-    var username = request.params.username;
-    var message = request.query["message"];
-    messages.push(username + ":" + message);
-    response.status(200);
-    response.send(messages);
-});
+var messages = {};
 
-app.get('/chatroom', function (request, response){
-    response.send(messages);
-})
+app.post('/chatroom/:roomName', function(request, response){
+    var roomName = request.params.roomName;
+    var username = request.query["username"]
+    var message = request.query["message"];
+   
+    if (!username || username == "") {
+      username = "Guest";
+    } // Make sure there is a username
+    
+    var messageValidPush = function(message, room){
+        if (message != undefined && message != "") {
+            if (messages[room] == undefined){
+                messages[room] = []; 
+            } 
+            messages[room].push(username + ":" + message);  
+        }  
+    }; // A function to validate message and create messages[room]
+    
+   if (roomName != undefined && roomName != ""){ 
+        messageValidPush(message, roomName);    
+   } //First if make sure roomName is defined
+    
+    var chatRoomJson = {
+        "room": {
+          "name": roomName,
+          "messages": messages[roomName]    
+          }
+    };
+    response.status(200);
+    response.send(chatRoomJson);
+});
 
 app.listen(56000, function (){
     console.log('Example app listening on port 56000')

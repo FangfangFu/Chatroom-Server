bdacae599d0d718db04711a51832710ce77346a1
Fangfang Fu <fangfang@fufangfang.com>
2016-03-13 00:07:52 -0600

    Add members and expireAfter query

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
 var express = require('express');
 var app = express();
 var messages = {};
-
-app.post('/chatroom/:roomName', function(request, response){
+var maxMessagePerRoom = 1000;
+var defaultMemberExpiration = 5*60;
+var members = {};
+ 
+app.post('/api/v1/chatroom/:roomName', function(request, response){
     var roomName = request.params.roomName;
     var username = request.query["username"]
     var message = request.query["message"];
+    var expireAfter = request.query["expireafter"];
    
+   // Make sure there is a username
+    
+    if (!expireAfter) {
+      expireAfter = defaultMemberExpiration;
+    }
     if (!username || username == "") {
       username = "Guest";
-    } // Make sure there is a username
+    } 
     
+    
+   // A function to validate message and create messages[room]
     var messageValidPush = function(message, room){
         if (message != undefined && message != "") {
             if (messages[room] == undefined){
                 messages[room] = []; 
             } 
-            messages[room].push(username + ":" + message);  
+            messages[room].push(username + ": " + message);  
         }  
-    }; // A function to validate message and create messages[room]
+    }; 
     
-   if (roomName != undefined && roomName != ""){ 
+    //First if make sure roomName is defined
+    if (roomName != undefined && roomName != ""){ 
         messageValidPush(message, roomName);    
-   } //First if make sure roomName is defined
-    
+    }
+     
+    if (members[roomName] == undefined){
+        members[roomName] = [];
+    } 
+    var membersString = updateMembers(members[roomName], username, expireAfter);
+    if (membersString == undefined) {membersString = "";}
+
     var chatRoomJson = {
         "room": {
           "name": roomName,
+          "members": membersString,
           "messages": messages[roomName]    
           }
     };
@@ -37,3 +56,47 @@ app.post('/chatroom/:roomName', function(request, response){
 app.listen(56000, function (){
     console.log('Example app listening on port 56000')
 })
+
+function updateMembers(array, username, expireAfter){
+    var currentTimeStamp = Date.now() + expireAfter * 1000; 
+    var member = username + ":" + currentTimeStamp;
+    noRepeatUsername(array, username);
+    if(expireAfter != 0){
+            array.push(member);
+    }
+    
+    checkExpireMember(array);
+    var string = convertArraytoString(array);
+    return string;
+}
+
+function noRepeatUsername(array, username){
+    for (var i = 0; i < array.length; i++){
+        var arrayMember = array[i].split(":")[0];
+        var arrayTime = array[i].split(":")[1];
+        if(arrayMember == username) {
+            array.splice(i, 1);
+        } 
+    }
+}
+function checkExpireMember(array){
+    for (var i = 0; i < array.length; i++){
+        var arrayMember = array[i].split(":")[0];
+        var arrayTime = array[i].split(":")[1];
+        if (arrayTime < Date.now()) {
+            array.splice(i, 1);
+        } 
+    }
+}
+function convertArraytoString(array){
+    var resultString = "";
+    if (array != undefined && array != []){
+        resultString = array[0];
+        for (i = 1; i < array.length; i++){
+            resultString += "," + array[i];
+        }       
+    }
+    return resultString;
+}
+
+
